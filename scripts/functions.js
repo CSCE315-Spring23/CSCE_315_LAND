ae0e2b4cb08bef1fe29ea301981f3d17a51ed556
jonasaustinland <jonasaustinland@tamu.edu>
2023-02-02 13:39:27 -0600

    Added alt-style.css and now you can switch between them

---
+++ scripts/functions.js
@@ -0,0 +1,50 @@
+
+function toggleStyleSheet(){
+    // Task 1
+    // Steps
+    // 1 (a) Get style element by ID (hint: getElementById)
+    var myStyle = document.getElementById("mainStyleSheet");
+
+    // 1 (b) Check the current stylesheet file name. (hint: element.getAttribute)
+    const myStyleName =  myStyle.getAttribute("href");
+    
+    // 1 (c) Determine new stylesheet file name
+
+    var myNewStyle = "styles/alt-style.css";
+    
+    if(myStyleName == "styles/alt-style.css"){
+        myNewStyle = "styles/style.css";
+        
+    }
+    // 1 (d) replace stylesheet with new stylesheet (hint: element.setAttribute)
+    myStyle.setAttribute("href",myNewStyle);
+
+   
+    
+    
+
+    // TASK 2
+    // 2 (d) For persistence when page is refreshed. save new stylesheet name to localStorage
+    // hint: localStorage.setItem(name, value)
+    localStorage.setItem("myStyle", myNewStyle);
+}
+
+
+window.onload = function(){
+    // TASK 2
+    // TODO: Make the last div color persist even when someone refreshes the page.
+
+    // Steps
+    // 2 (a) get stylesheet name from local storage hint: localStorage.getItem(name)
+    var mySavedStyle = localStorage.getItem("myStyle");
+    // 2 (b) get html style element by ID
+    var myStyle = document.getElementById("mainStyleSheet");
+    // 2 (c) replace href attribute of html element.
+    if(mySavedStyle !=null){
+        myStyle.setAttribute("href",mySavedStyle);
+    }
+    
+    
+}
+
+
